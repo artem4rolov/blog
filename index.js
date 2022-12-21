b2a6bfd064bb6ddffd300df52d3ec908268e7d8d
Artem <artem4rolov@gmail.com>
2022-12-21 22:16:42 +0300

    сгенерирваоли токен авторизации пользователя, зашифровали пароль, провели валидацию данных пользователя, сохранили пользователя в БД MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // библиотека express для работы с сервером на Node.js
 import express from "express";
+// библиотека express-validator для валидации данных, отправляемых пользователем
+import { validationResult } from "express-validator";
+// библиотека jwt для создания токенов авторизации пользователя
 import jwt from "jsonwebtoken";
+// библиотека bcrypt для шифрования пароля (вообще это для бэка больше, чем для фронта)
+import bcrypt from "bcrypt";
+// библиотека mongoose для работы с MongoDB
 import mongoose from "mongoose";
+
+import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
 
 // подключаемся к БД mongoDb (чтобы зайти на сатй- нужно включать VPN!)
 mongoose
@@ -26,8 +35,33 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// при отправке данных на сервер
-app.post("/auth/register", (req, res) => {});
+// при отправке данных на сервер, валидируем их, и если все ок - продолжаем
+app.post("/auth/register", registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // если наши ошибки есть - возвращаем наш массив ошибок с кодом 400 (ошибка на стороне пользователя)
+    return res.status(400).json(errors.array());
+  }
+
+  // получаем пароль пользователя
+  const password = req.body.passwordHash;
+  // алгоритм шифрования в bcrypt (этот способ шифрования используют многие компании)
+  const salt = await bcrypt.genSalt(10);
+  // шифруем пароль пользователя
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatatUrl: req.body.avatatUrl,
+    passwordHash,
+  });
+
+  // сохраняем данные пользователя в базе MongoDB
+  const user = await doc.save();
+
+  res.json(user);
+});
 
 // указываем порт, на котором будет запущен сервер (localhost:4444) и вывод ошибки, если не запустится
 app.listen(4444, (err) => {
